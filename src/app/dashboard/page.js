1276796238e987f6c1971f4a55f1c7d9df2d1768
Tom <tomfgreening@gmail.com>
2026-06-11 16:19:39 +0100

    Created dashboard page. Confirmed navigation to page works.

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+export default function DashboardPage () {
+    return(
+        <>
+        <h1>Welcome back, USER </h1>
+        <h2>Track previous avacados, or check a new one. </h2>
+
+        [ add avacado ]
+        [ your tracked avacados ]
+        [ no avacoads tracked ]
+        </>
+    );
+}
