cbc36cc7d82bea679ac57b29bc96a3654daf3bf8
Tom <tomfgreening@gmail.com>
2026-06-11 13:10:20 +0100

    Cleared template data. Moved layout and page components to Components folder.

---
+++ src/Components/layout.js
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "RiPEN",
+  description: "it knows before you do",
+};
+
+export default function RootLayout({  }) {
+  return (
+  );
+}
