22cc695b00a238a0d18ff3cd6bfd7db56b19c4f4
Tom <tomfgreening@gmail.com>
2026-06-12 16:25:31 +0100

    Confirmed avocado data from form is readable in dashboard.

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,18 +1,17 @@
 import { SignOutButton } from "@clerk/nextjs";
+import db from "@/utils/dbConnection";
 
-
-export default function DashboardPage () {
-    return(
+export default async function DashboardPage () {
+    const avocados = await db.query('SELECT * FROM avocados');
+    const wrangledAvocados = avocados.rows;
+    console.log(wrangledAvocados);
+        return (
         <>
         <h1>Welcome back, USER </h1>
-        <h2>Track previous avacados, or check a new one. </h2>
-
-        [ add avacado ]
-        [ your tracked avacados ]
-        [ no avacoads tracked ]
-        <SignOutButton>
+        <h2>Track previous avocados, or check a new one. </h2>
+         {/* avocado data to be displayed here */}
+         <SignOutButton>
             <button>Sign out.</button>
-        </SignOutButton>
-        </>
+        </SignOutButton></>
     );
 }
