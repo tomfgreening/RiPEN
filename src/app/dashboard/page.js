4a1c5463980444bac33d779e9828433f588af0c2
Tom <tomfgreening@gmail.com>
2026-06-26 16:09:25 +0100

    Imported auth functionality from clerk to dashboard page.

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,17 +1,19 @@
 import { SignOutButton } from "@clerk/nextjs";
 import db from "@/utils/dbConnection";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function DashboardPage () {
+    const { userId } = await auth();
     const avocados = await db.query('SELECT * FROM avocados');
     const wrangledAvocados = avocados.rows;
     console.log(wrangledAvocados);
         return (
         <>
-        <h1>Welcome back, USER </h1>
+        <h1>Welcome back, { userId } </h1>
         <h2>Track previous avocados, or check a new one. </h2>
          {/* avocado data to be displayed here */}
          <SignOutButton>
             <button>Sign out.</button>
         </SignOutButton></>
     );
-}
+}       
