f95df728e9ec63e2c77e5ef2a9c1d88c0b4100f2
Tom <tomfgreening@gmail.com>
2026-07-22 11:06:01 +0100

    Created page for nested route to allow users to update an existing avocado.

---
+++ src/app/update-avocado/page.js
@@ -0,0 +1,5 @@
+import db from "@/utils/dbConnection";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default async function UpdateAvocadoPage(){} 
