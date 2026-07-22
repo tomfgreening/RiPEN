0de5f81f9c50db4786a91314c0be8c04663dfdcd
Tom <tomfgreening@gmail.com>
2026-07-22 11:46:03 +0100

    Created function to to get avocados from database with UUID

--- src/app/update-avocado/page.js
+++
@@ -1,5 +0,0 @@
-import db from "@/utils/dbConnection";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
-export default async function UpdateAvocadoPage(){} 
